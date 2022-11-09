1b9458a6b0fc02f60dd01f0d7400990f16de5048
nall1 <110547679+nall1@users.noreply.github.com>
2022-11-09 13:22:59 -0600

    coding test changes

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,6 +1,9 @@
 package com.aa.act.interview.org;
-
+ 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public abstract class Organization {
 
@@ -20,15 +23,35 @@ public abstract class Organization {
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
+		 
+		  List<Position> collect = root.getDirectReports().stream().flatMap(p->Stream.concat(Stream.of(p), p.getDirectReports()
+				  .stream().flatMap(x->Stream.concat(Stream.of(x), x.getDirectReports().stream())))).collect(Collectors.toList());
+		  	collect.add(root);
+		    Position position = collect.stream().filter(x->x.getTitle().equalsIgnoreCase(title)).findAny().orElse(null);
+		//your code here		   
+ 
+		  if(position !=null) {
+			  try {
+				  if(!position.isFilled()) {				  
+					  Optional<Employee> emp = Optional.of(new Employee(position.hashCode(), person));
+					  position.setEmployee(emp);
+				  }
+				 
+				 return Optional.of(position);
+			  }catch(Exception e) {
+					return Optional.empty();
+			  }
+		  }
+		 
 		return Optional.empty();
 	}
-
+  
 	@Override
 	public String toString() {
 		return printOrganization(root, "");
 	}
 	
+	 
 	private String printOrganization(Position pos, String prefix) {
 		StringBuffer sb = new StringBuffer(prefix + "+-" + pos.toString() + "\n");
 		for(Position p : pos.getDirectReports()) {
